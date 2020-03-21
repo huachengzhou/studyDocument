a5939cd4f5d0cc64fbf73719d966dfa541d29d27
zhouchenhua <noatnu@163.com>
2020-03-21 23:23:38 +0800

    vue 练习

--- resume/blog/example/vue/nav.js
+++ resume/blog/example/vue/nav.js
@@ -1,4 +1,30 @@
 
+function getCurrentDate() {
+    var now = new Date();
+    var year = now.getFullYear(); //得到年份
+    var month = now.getMonth();//得到月份
+    var date = now.getDate();//得到日期
+    var day = now.getDay();//得到周几
+    var hour = now.getHours();//得到小时
+    var minu = now.getMinutes();//得到分钟
+    var sec = now.getSeconds();//得到秒
+    var MS = now.getMilliseconds();//获取毫秒
+    var week;
+    month = month + 1;
+    if (month < 10) month = "0" + month;
+    if (date < 10) date = "0" + date;
+    if (hour < 10) hour = "0" + hour;
+    if (minu < 10) minu = "0" + minu;
+    if (sec < 10) sec = "0" + sec;
+    if (MS < 100)MS = "0" + MS;
+    var arr_week = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    week = arr_week[day];
+    var time = "";
+    time = year + "年" + month + "月" + date + "日" + " " + hour + ":" + minu + ":" + sec + " " + week;
+    //当前日期赋值给当前日期输入框中（jQuery easyUI）
+   return time ;
+}
+
 function exampleData() {
     var data = [];
     var len = 10;
@@ -22,6 +48,11 @@ function exampleData() {
         id: ++id,
         name: "hello world",
         href: "exercise/helloword.html"
+    });
+    data.unshift({
+        id: ++id,
+        name: "2020 03 21",
+        href: "exercise/2020_03_21.html"
     });
     return data;
 }
